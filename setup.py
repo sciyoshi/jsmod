242ae3d2ccd6175af35609125a25543a4fe916d1
Samuel Cormier-Iijima <sam@fluidware.com>
2014-09-23 01:28:29 -0400

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='jsmod',
-	version='0.0.1',
+	version='0.0.2',
 	description='Import JavaScript modules directly from Python code',
 	url='https://github.com/sciyoshi/jsmod/',
 	author='Samuel Cormier-Iijima',
